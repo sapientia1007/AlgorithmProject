09deda6e589a994588ca257e4824a285b30e570f
jjhcom <91904494+jjhcom@users.noreply.github.com>
2024-04-16 13:51:04 +0900

    [Silver V] Title: 단어 나누기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.py
@@ -0,0 +1,13 @@
+import sys
+word = list(map(str, sys.stdin.readline().strip()))
+res = []
+for i in range(1, len(word)-1) :
+    for j in range(i+1, len(word)) :
+        first = word[:i]
+        second = word[i:j]
+        third = word[j:]
+        first.reverse()
+        second.reverse()
+        third.reverse()
+        res.append("".join(first+second+third))
+print(min(res))
