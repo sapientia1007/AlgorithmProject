4f18232c7d2907e2b93b95c4fcc0cdf916005535
sapientia1007 <91904494+sapientia1007@users.noreply.github.com>
2024-07-09 20:57:01 +0900

    [Silver III] Title: 회전하는 큐, Time: 128 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+/* 입력
+    N : 큐의 크기 / M : 뽑아내려고 하는 수의 개수
+    지민이가 뽑아내려고 하는 수의 위치가 순서대로
+
+    ex) 1 2 3 4 5 6 7 8 9 10 // 1 2 3
+
+ */
+public class Main {
+    static int N; static int M; static int count = 0;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken()); M = Integer.parseInt(st.nextToken());
+        LinkedList<Integer> q = new LinkedList<>();
+        for (int i=1; i<=N; i++) q.add(i);
+        int[] arr = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<M; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());// 뽑아내려고 하는 숫자
+            int target_idx = q.indexOf(arr[i]); // 목표하는 숫자 인덱스 저장
+
+            int half_idx;
+
+            if (q.size() % 2 == 0){
+                half_idx = q.size()/2-1;
+            } else {
+                half_idx = q.size()/2;
+            }
+
+            if (target_idx <= half_idx) { // 이진으로 볼 때 앞쪽 배열기준 -> 앞에서 빼기
+                for (int j = 0; j < target_idx; j++) {
+                    int temp = q.pollFirst();
+                    q.offerLast(temp);
+                    count++;
+                }
+            } else {
+                for (int j=0; j<q.size()-target_idx; j++) { // 뒤쪽 배열 기준 -> 뒤에서 빼기
+                    int temp = q.pollLast();
+                    q.offerFirst(temp);
+                    count++;
+                }
+            }
+            q.pollFirst(); // 맨앞쪽 요소 제거
+        }
+        System.out.println(count);
+        br.close();
+    }
+}
