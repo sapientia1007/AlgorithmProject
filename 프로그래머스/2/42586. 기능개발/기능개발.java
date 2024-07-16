b693336ecd60d6d79947cf27ac3188f129bdba54
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2024-07-16 21:28:44 +0900

    [level 2] Title: 기능개발, Time: 0.18 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,13 +1,14 @@
 import java.io.*;
 import java.util.*;
+
 class Solution {
         public int[] solution(int[] progresses, int[] speeds) {
             int[] answer = {};
-
+            // 결과를 담을 arrayList
             ArrayList<Integer> answerLst = new ArrayList<>();
             ArrayDeque<Integer> q = new ArrayDeque<>();
 
-            // 남은 일수 계산 - 속도로 몇일나오는지 계산
+            // 남은 일수 계산 - 속도로 몇일나오는지 계산 (큐)
             for (int i=0; i<progresses.length; i++) {
                 int day = 0;
                 if ((100-progresses[i])%speeds[i] == 0) {
@@ -20,22 +21,23 @@ class Solution {
             }
 
             int cnt = 1;
-            int num = q.pollFirst(); // 일단 첫번째 값 기준 정하기 위해 q에서 빼기
+            int num = q.pollFirst(); // 일단 첫번째 값 기준 정하기 위해 q에서 poll -> 첫번째 남은 일 수
 
-            // 앞에 있는 기능보다 뒤에 있는 기능이 먼저 개발되더라도, 뒤에 있는 기능은 앞에 있는 기능이 배포될때 함께 => 비교
+            // 비교
             while (true) {
                 if (q.isEmpty()) break;
-                if (num>=q.peekFirst()) {
+                if (num>=q.peekFirst()) { // 현재 작업의 남은 일수가 이전 작업의 남은 일수보다 작거나 같으면 같이 배포
                     q.pollFirst();
                     cnt +=1 ;
-                } else if (q.peekFirst()>num){
+                } else if (q.peekFirst()>num){ // 현재 작업의 남은 일수가 이전 작업의 남은 일수보다 크면 배포 처리 => 새로운 기준 + cnt초기화
                     answerLst.add(cnt);
                     num = q.pollFirst();
                     cnt=1;
                 }
             }
-            answerLst.add(cnt);
+            answerLst.add(cnt); // 마지막으로 남은 작업 수
 
+            // arrayList를 배열로 변환
             answer = new int[answerLst.size()];
             for (int i=0; i<answerLst.size(); i++) {
                 answer[i] = answerLst.get(i);
