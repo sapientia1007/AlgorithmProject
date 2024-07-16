79e36b6639500265b107e068f61022f58d56ba52
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2024-07-16 21:06:23 +0900

    [level 2] Title: 올바른 괄호, Time: 18.92 ms, Memory: 52.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,26 +2,26 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-	    boolean solution(String s) throws IOException {
-	        boolean answer = true;
+        boolean solution(String s) throws IOException {
+            boolean answer = true;
 
-	        ArrayDeque<Character> q = new ArrayDeque<>();
-	        
-	        for (int i=0; i<s.length(); i++) {
-	        	char inpt = s.charAt(i);
-	        	
-	        	if (q.isEmpty()) {
-	        		q.offer(inpt);
-	        	} else if (inpt == ')' && q.peekLast() == '(') {
-	        		q.pollLast();
-	        	} else if (inpt == ')' && q.isEmpty()) {
-	        		answer = false;
-	        		break;
-	        	} else if (inpt == '('){
-	        		q.offer(inpt);
-	        	}
-	        }
-	        if (q.size() != 0) answer = false;
-	        return answer;
-	    }
-	}
+            ArrayDeque<Character> st = new ArrayDeque<>();
+
+            for (int i=0; i<s.length(); i++) {
+                char inpt = s.charAt(i);
+
+                if (st.isEmpty()) {
+                    st.add(inpt);
+                } else if (inpt == ')' && st.peekLast() == '(') { // )일때 스택에 (가 들어있으면, 빼버리기
+                    st.pop();
+                } else if (inpt == ')' && st.isEmpty()) { // )일때 비어있으면 false
+                    answer = false;
+                    break;
+                } else if (inpt == '('){ // (일때는 스택에 추가
+                    st.add(inpt);
+                }
+            }
+            if (st.size() != 0) answer = false; // 스택이 비어있지 않으면 괄호가 맞지 않는 것 -> false
+            return answer;
+        }
+    }
