bea412283767ef59430ba6811fb4cc30ad4e4683
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2024-07-16 21:03:12 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 38.29 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,28 +2,27 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-	    public int[] solution(int[] arr) {
-	      
-	        // 큐 생성
-	        ArrayDeque<Integer> q = new ArrayDeque<>();
-	        
-	        q.add(arr[0]); // 일단 하나 저장
-	        
-	        // arr 배열 내 숫자 중 q의 가장 최근에 들어간 숫자와 같지 않다면 추가
-	        for (int a : arr) {
-	        	if (q.peekLast() != a) {
-					q.offer(a);
-				}
-	        }
+        public int[] solution(int[] arr) {
 
-	       	int[] answer = new int[q.size()]; 
-	       
-			int idx = 0;
-	       // 배열 저장
-			while(!q.isEmpty()){
-				answer[idx++] = q.poll();
-			}
-	        return answer;
-	    }
-	}
-	
+            // 스택 생성
+            ArrayDeque<Integer> s = new ArrayDeque<>();
+
+            s.add(arr[0]); // 일단 하나 저장
+
+            // arr 배열 내 숫자 중 s의 가장 최근에 들어간 숫자와 같지 않다면 추가
+            for (int a : arr) {
+                if (s.peekLast() != a) {
+                    s.add(a);
+                }
+            }
+
+            int[] answer = new int[s.size()];
+
+            int idx = 0;
+            // 배열 저장
+            while(!s.isEmpty()){
+                answer[idx++] = s.pop();
+            }
+            return answer;
+        }
+    }
