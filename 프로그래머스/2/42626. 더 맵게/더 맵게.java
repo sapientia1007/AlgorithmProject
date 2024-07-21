1a872d85305b2011f7def538fac451f0fb98a2a3
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2024-07-21 15:42:49 +0900

    [level 2] Title: 더 맵게, Time: 1324.56 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,23 +1,23 @@
 import java.util.*;
 
 class Solution {
-   public int solution(int[] scoville, int K) {
+    public int solution(int[] scoville, int K) {
         int answer = 0;
 
-        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         for (int s : scoville) {
-            heap.add(s);
+            pq.add(s); // priorityqueue에 scoville 배열 담기
         }
 
-        while (heap.size()>1 && heap.peek()<K){
-            int firstNum = heap.poll();
-            int secondNum = heap.poll();
+        while (pq.size()>1 && pq.peek()<K){ // 2개 이상의 heap 배열이고, 첫번째 값이 K보다 작으면 -> 섞은 음식의 스코빌 지수 리턴
+            int firstNum = pq.poll();
+            int secondNum = pq.poll();
             int mixedScoville = firstNum + (secondNum*2);
-            heap.add(mixedScoville);
+            pq.add(mixedScoville);
             answer++;
         }
 
-        if (heap.peek()<K) answer=-1;
+        if (pq.peek()<K) answer=-1; // 배열 값이 부족한데, 해당 첫번째값이 K보다 작으면, 스코빌 지수를 K이상으로 만들기 불가
 
         return answer;
     }
