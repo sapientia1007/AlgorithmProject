8da46bc45faf0e22180873c7bad415e23fe35c19
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2024-07-31 20:11:44 +0900

    [level 1] Title: 최소직사각형, Time: 1.44 ms, Memory: 99.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -2,30 +2,16 @@ class Solution {
         public int solution(int[][] sizes) {
             int answer = 0;
 
-            int[][] resSizes = new int[sizes.length][2];
+            int MaxWidth = 0; int MaxHeight = 0;
 
-            // 배열 정렬
-            for (int i=0 ; i<sizes.length; i++) {
-                if (sizes[i][0] >= sizes[i][1]) {
-                    resSizes[i][0] = sizes[i][0];
-                    resSizes[i][1] = sizes[i][1];
-                } else {
-                    resSizes[i][0] = sizes[i][1];
-                    resSizes[i][1] = sizes[i][0];
-                }
-            }
-            
-            int max = Integer.MIN_VALUE; int max2 = Integer.MIN_VALUE;
-            for (int i=0 ; i<resSizes.length; i++) {
-                if (max < resSizes[i][0]) {
-                    max = resSizes[i][0];
-                }
-                if (max2 < resSizes[i][1]) {
-                    max2 = resSizes[i][1];
-                }
+            for (int[] size : sizes){
+                int maxVal = Math.max(size[0],size[1]); // 큰값끼리 비교하기 위함
+                int minVal = Math.min(size[0], size[1]); // 작은값끼리 비교하기 위함
+                MaxWidth = Math.max(maxVal, MaxWidth);
+                MaxHeight = Math.max(minVal, MaxHeight);
             }
 
-            answer = max*max2;
+            answer = MaxWidth*MaxHeight;
             return answer;
         }
     }
