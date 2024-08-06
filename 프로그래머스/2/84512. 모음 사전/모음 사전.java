b6d1e2b957155bf4b985ae872f814fb401ef839b
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2024-08-07 08:58:16 +0900

    [level 2] Title: 모음 사전, Time: 7.57 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -17,7 +17,7 @@ class Solution {
         }
         public void dfs(String str, int len) {
             list.add(str); // 현재 문자열 추가
-            if (len == 5) return; // 문자열 길이가 5이면 종료 
+            if (len == 5) return; // 문자열 길이가 5이면 종료 (최대문자열길이=5)
             for (int i=0; i<5; i++) { // 모든 모음을 현재 문자열에 추가하여 새로운 문자열 생성하는 재귀 호출
                 dfs(str+dict[i], len+1);
             }
