c760829caac7dcbea59e9ae5e2ca33fb76a49486
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2024-08-06 21:37:29 +0900

    [level 2] Title: 카펫, Time: 8.64 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
 
 ### 성능 요약
 
-메모리: 75.7 MB, 시간: 7.23 ms
+메모리: 80.1 MB, 시간: 8.64 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 05일 18:54:03
+2024년 08월 06일 21:37:23
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,15 +1,21 @@
- class Solution {
+class Solution {
         public int[] solution(int brown, int yellow) {
             int[] answer = new int[2];
-            // 전체 사각형 개수
+            // 전체 사각형 타일 개수
             int sum = brown+yellow;
 
             for (int i=3; i<=sum; i++) {
-                if (sum % i == 0){ // 현재 길이가 전체 사각형 개수의 약수인 경우
-                    int x = i;
-                    int y = sum / i;
-                    if ((x-2) * (y-2) == yellow) {
-                        answer[0] = x; answer[1] = y; // 노란색 타일의 개수는 (가로 길이 - 2) * (세로 길이 - 2)로 계산
+                if (sum % i == 0) { // 현재 i가 전체 사각형 개수의 약수인 경우
+                    int x = i; // 가로
+                    int y = sum / i; // 세로
+                    if ((x - 2) * (y - 2) == yellow) { // 노란색 타일의 개수 = 내부 타일 = (가로 길이 - 2) * (세로 길이 - 2)로 계산
+                        if (x >= y) {
+                            answer[0] = x; // 가로 길이
+                            answer[1] = y; // 세로 길이
+                        } else {
+                            answer[0] = y; // 가로 길이
+                            answer[1] = x; // 세로 길이
+                        }
                     }
                 }
             }
