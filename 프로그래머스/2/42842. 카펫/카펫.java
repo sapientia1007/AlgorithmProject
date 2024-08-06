6fd88b58725e9a97b1be748adb056c8dca5fcd9f
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2024-08-07 08:56:40 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -4,12 +4,12 @@ class Solution {
             // 전체 사각형 타일 개수
             int sum = brown+yellow;
 
-            for (int i=3; i<=sum; i++) {
+            for (int i=3; i<=Math.sqrt(sum); i++) {
                 if (sum % i == 0) { // 현재 i가 전체 사각형 개수의 약수인 경우
                     int x = i; // 가로
                     int y = sum / i; // 세로
                     if ((x - 2) * (y - 2) == yellow) { // 노란색 타일의 개수 = 내부 타일 = (가로 길이 - 2) * (세로 길이 - 2)로 계산
-                        if (x >= y) {
+                        if (x >= y) { // 가로 > 세로
                             answer[0] = x; // 가로 길이
                             answer[1] = y; // 세로 길이
                         } else {
