4dd2fcab3ff9ce49011184b5c60066f84c60ca00
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2024-08-07 09:14:10 +0900

    [level 2] Title: 카펫, Time: 0.12 ms, Memory: 65.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -9,13 +9,7 @@ class Solution {
                     int x = i; // 가로
                     int y = sum / i; // 세로
                     if ((x - 2) * (y - 2) == yellow) { // 노란색 타일의 개수 = 내부 타일 = (가로 길이 - 2) * (세로 길이 - 2)로 계산
-                        if (x >= y) { // 가로 > 세로
-                            answer[0] = x; // 가로 길이
-                            answer[1] = y; // 세로 길이
-                        } else {
-                            answer[0] = y; // 가로 길이
-                            answer[1] = x; // 세로 길이
-                        }
+                        answer[0] = Math.max(x,y); answer[1] = Math.min(x,y);
                     }
                 }
             }
