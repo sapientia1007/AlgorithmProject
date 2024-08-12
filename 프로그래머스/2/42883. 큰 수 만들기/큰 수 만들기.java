c9a40df1f7305279e3d2cc5730e8ab6c6ffe4188
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2024-08-12 21:12:33 +0900

    [level 2] Title: 큰 수 만들기, Time: 5818.25 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,28 @@
+class Solution {
+        public String solution(String number, int k) {
+//            String answer = "";
+            StringBuilder answer = new StringBuilder();
+            int numLen = number.length() - k; // 만들어야 할 숫자 길이
+            int idx = 0;
+
+            while (answer.length() < numLen) {
+                char maxNum = '0';
+                int endIdx = number.length() - numLen + answer.length();
+                int maxIdx = idx;
+
+                for (int i = idx; i <= endIdx; i++) {
+                    char currentChar = number.charAt(i);
+                    if (currentChar > maxNum) {
+                        maxNum = currentChar;
+                        maxIdx = i;
+                    }
+                }
+
+                answer.append(maxNum);
+                idx = maxIdx + 1;
+                k -= maxIdx - (idx - 1);
+            }
+
+            return answer.toString();
+        }
+    }
