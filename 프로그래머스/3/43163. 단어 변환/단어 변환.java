a6c867762527e083418a125c652444a53f19bb56
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2024-08-27 21:17:51 +0900

    [level 3] Title: 단어 변환, Time: 0.37 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,32 +1,6 @@
 class Solution {
-        static boolean[] v;
-        static int answer = 0;
-        public int solution(String begin, String target, String[] words) {
-            v = new boolean[words.length];
-
-            dfs(begin, target, words, 0);
-            return answer;
-        }
-
-        static void dfs(String now, String target, String[] words, int cnt) {
-            if (now.equals(target)) { // 원하는 단어와 같아지면
-                answer = cnt;
-                return;
-            }
-
-            for (int i=0; i<words.length; i++) {
-                if (v[i] == true) continue; // 이미 방문한 단어면 pass
-
-                String word = words[i]; int notEqual = 0;
-                for (int j=0; j<now.length(); j++) {
-                    if (now.charAt(j) != words[i].charAt(j)) notEqual++; // 단어 별 다른 알파벳 개수
-                }
-                // 1개만 다르면
-                if (notEqual == 1){
-                    v[i] = true;
-                    dfs(word, target, words, cnt+1);
-                    v[i] = false; // 방문 초기화
-                }
-            }
-        }
+    public int solution(String begin, String target, String[] words) {
+        int answer = 0;
+        return answer;
     }
+}
