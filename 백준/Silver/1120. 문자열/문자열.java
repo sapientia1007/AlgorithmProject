07bde5ef6478783d12da4e5ade85927c594b33f8
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2024-09-04 22:20:22 +0900

    [Silver IV] Title: 문자열, Time: 108 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Silver/1120. 문자열/문자열.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        String a = st.nextToken();
+        String b = st.nextToken();
+
+        int minCnt = Integer.MAX_VALUE;
+
+        for (int i=0; i<= b.length()-a.length(); i++) {
+            int cnt = 0;
+            for (int j=0; j < a.length(); j++) {
+                if (a.charAt(j) != b.charAt(i+j)){
+                    cnt ++;
+                }
+            }
+            minCnt = Math.min(cnt,minCnt);
+        }
+        System.out.println(minCnt);
+        br.close();
+    }
+}
