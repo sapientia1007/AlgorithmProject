f9e60683fbe101e40c356b7bbb32ef0cbedf1e3d
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2024-09-16 23:48:26 +0900

    [Silver II] Title: N과 M (9), Time: 388 ms, Memory: 35436 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,11 +1,11 @@
 import java.util.*;
 import java.io.*;
 
-public class Main{
+public class Main {
     static int N, M;
     static int[] arr, pick;
     static boolean[] v;
-    static Set<String> res = new HashSet<>();
+    static Set<String> res = new HashSet<>(); // 정답 배열
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] inpt = br.readLine().split(" ");
@@ -22,28 +22,28 @@ public class Main{
         v = new boolean[N]; // 사용 유무 배열
 
         Arrays.sort(arr); // 순서를 유지하기 위한 정렬
-        makeNumber(0,0);
+
+        makeNumber(0);
 
         br.close();
     }
-
-    static void makeNumber(int depth, int start) {
+    static void makeNumber(int depth) {
         if (depth == M) {
             StringBuilder sb = new StringBuilder();
             for (int i=0; i<M; i++) {
                 sb.append(pick[i]).append(" ");
             }
             if (!res.contains(sb.toString())) {
-                System.out.println(sb.toString());
                 res.add(sb.toString());
+                System.out.println(sb.toString());
             }
             return;
         }
-        for (int i =0; i<N; i++) {
+        for (int i=0; i<N; i++) {
             if (!v[i]) {
                 v[i] = true;
                 pick[depth] = arr[i];
-                makeNumber(depth+1, i+1);
+                makeNumber(depth+1);
                 v[i] = false;
             }
         }
