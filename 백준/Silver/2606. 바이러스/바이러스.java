016b58e68d85e0914c0132d53b8fb6ef4d375735
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2025-01-04 00:05:36 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,51 +1,51 @@
 import java.io.*;
 import java.util.*;
-/*
- * 입력 : 
- * 		N : 컴퓨터의 수
- * 		M : 네트워크 상에서 직접 연결되어 있는 컴퓨터 쌍의 수
- * 		M개의 줄 - 네트워크 상에서 직접 연결되어 있는 컴퓨터의 번호 쌍
- * 출력 : 
- * 		1번 컴퓨터에 의해 바이러스가 걸리게 되는 컴퓨터의 수
- * 처리 : 
- * 		bfs 사용
- */
+
 public class Main {
-	static int N, M ;
-	static List<Integer> [] g;
-	static boolean[] v;
-	static ArrayList<Integer> res = new ArrayList<>();
-		
-	static void bfs(int i) {
-		ArrayDeque<Integer> q = new ArrayDeque<>();
-		v[i] = true;
-		q.offer(i);
-		while (!q.isEmpty()) {
-			i = q.poll();
-			res.add(i);
-			for (int j : g[i]) {
-				if (!v[j]) {
-					v[j] = true;
-					q.offer(j);
-				}
-			}
-		}
-	}
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine()); M = Integer.parseInt(br.readLine());
-		g = new List[N+1]; for (int i=1; i<N+1; i++) g[i] = new ArrayList<>();
-		v = new boolean[N+1];
-		for (int i=0; i<M; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			g[from].add(to);
-			g[to].add(from);
-		}
-		bfs(1);
-		System.out.println(res.size()-1);
-		br.close();
-	}
+    static boolean[] v;
+    static ArrayList<Integer>[] network;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine()); // 컴퓨터의 수
+
+        network = new ArrayList[N + 1];
+        v = new boolean [N+1];
+
+        int M = Integer.parseInt(br.readLine()); // 네트워크 상에서 직접 연결되어 있는 컴퓨터 쌍의 수
+
+        for (int i=1; i<=N; i++){
+            network[i] = new ArrayList<>();
+        }
+
+        for (int i=0; i<M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int fisrt = Integer.parseInt(st.nextToken()); int second = Integer.parseInt(st.nextToken());
+            network[fisrt].add(second); network[second].add(fisrt);
+        }
+
+        int cnt = dfs(1);
+        System.out.println(cnt);
+        br.close();
+    }
+
+    public static int dfs(int index) {
+        ArrayDeque<Integer> s = new ArrayDeque<>();
+        s.add(index);
+        v[index] = true;
+        int count = 0;
+
+        while (!s.isEmpty()){
+            int currnet = s.poll();
+
+            for (int neighbor: network[currnet]) {
+                if (!v[neighbor]) {
+                    v[neighbor] = true;
+                    s.add(neighbor);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 }
