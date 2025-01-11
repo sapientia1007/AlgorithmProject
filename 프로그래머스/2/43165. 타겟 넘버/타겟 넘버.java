e38124a92fb747755944f5c9716ec72093d3a45f
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2025-01-11 20:24:44 +0900

    [level 2] Title: 타겟 넘버, Time: 126.57 ms, Memory: 166 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,17 +1,31 @@
-class Solution {
-        static int answer = 0;
-        public int solution(int[] numbers, int target) {
-            answer = 0;
-            dfs(numbers,target,0,0);
-            return answer;
-        }
+import java.util.ArrayDeque;
 
-        public void dfs(int[] numbers, int target, int idx, int sum) { // 모든 경우를 탐색하기 위한 dfs
-            if (idx == numbers.length){ // numbers 배열 끝에 인덱스가 도달했으면
-                if (target == sum) answer++; // 협재 합이 타겟과 일치하면 경우의 수 증가 
-                return; // 탐색 종료
-            }
-            dfs(numbers, target, idx+1, sum+numbers[idx]); // 현재 수 더하기
-            dfs(numbers, target, idx+1, sum-numbers[idx]); // 현재 수 뺴기
-        }
+class Solution {
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+
+        // 모든 경우를 탐색
+        answer = dfs(numbers, target, 0, 0);
+        return answer;
     }
+    
+        public static int dfs(int[] numbers, int target, int idx, int sum) {
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        int answer = 0;
+        q.offer(new int[] {idx, sum});
+
+        while (!q.isEmpty()) {
+            int[] current = q.poll();
+            int num = current[0]; int result = current[1];
+
+            if (num == numbers.length) {
+                if (result == target) answer ++;
+            } else {
+                q.offer(new int[] {num+1, result + numbers[num]});
+                q.offer(new int[] {num+1, result - numbers[num]});
+            }
+        }
+        return answer;
+    }
+}
+
