3d66e22478d799579eb5f4e6badb106869f8a885
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2025-01-12 17:24:32 +0900

    [level 3] Title: 네트워크, Time: 0.35 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,41 +1,35 @@
 import java.util.*;
 
 class Solution {
-        int[] parent ;
-        public int find(int a){
-            if (parent[a] == a) return a;
-            return parent[a] = find(parent[a]);
-        }
-
-        public void union(int a, int b){
-            a = find(a); b = find(b);
-            if (a!=b) parent[b] = a;
-        }
-
         public int solution(int n, int[][] computers) {
-            int answer = 1;
-            parent = new int[n];
-            // 일단 부모 자기자신으로 담기
-            for (int i=0; i<n; i++) parent[i] = i;
+            int answer = 0;
 
-            for (int i = 0; i < computers.length; i++) {
-                for (int j=0; j< computers[i].length; j++){
-                    if (computers[i][j] == 1){
-                        union(i, j);
+            boolean[] v = new boolean[n];
+
+
+            for (int i=0; i<n; i++) {
+                if (!v[i]) {
+                    bfs(i, n, computers, v);
+                    answer ++;
+                }
+            }
+            return answer;
+        }
+
+        public void bfs(int start, int n, int[][] computers, boolean[] v) {
+            ArrayDeque<Integer> q = new ArrayDeque<>();
+            q.offer(start);
+            v[start] = true;
+
+            while (!q.isEmpty()) {
+                int current = q.poll();
+
+                for (int i=0; i<n; i++) {
+                    if (computers[current][i] == 1 && !v[i]) {
+                        v[i] = true;
+                        q.offer(i);
                     }
                 }
             }
-
-            for (int i=0; i<n; i++){
-                find(i);
-            }
-            
-            HashSet<Integer> newtork = new HashSet<>();
-            for (int p : parent){
-                newtork.add(parent[p]);
-            }
-            answer = newtork.size();
-
-            return answer;
         }
     }
