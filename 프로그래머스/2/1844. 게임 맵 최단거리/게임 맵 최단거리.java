7ec031cb95fee698a606869e0b6d3183d6330f80
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2025-01-13 13:25:02 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.17 ms, Memory: 55.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,48 +1,48 @@
 import java.util.*;
 
 class Solution {
-        // 0 : 벽이 있는 자리 <-> 1: 벽이 없는 자리(이동 가능한 자리)
-        static int[] di = {-1,0,1,0};
-        static int[] dj = {0,1,0,-1};
-        static boolean[][] v ;
-        static int row; static int col;
+
+        static int[] di = new int[] {-1, 0, 0, 1};
+        static int[] dj = new int[] {0, 1, -1, 0};
+        static boolean[][] v;
+        static int m, n;
+
         public int solution(int[][] maps) {
             int answer = 0;
+            m = maps.length;
+            n = maps[0].length;
 
-            row = maps.length; col = maps[0].length;
+            v = new boolean[m][n];
 
-            v = new boolean[row][col]; // 방문 여부
-
-            answer = bfs(maps,0, 0); // 시작점
+            answer = bfs(maps, 0, 0);
 
             return answer;
         }
-        public int bfs(int[][] maps, int i, int j){
+
+        public int bfs(int[][] maps, int row, int col) {
             ArrayDeque<int[]> q = new ArrayDeque<>();
-            q.offer(new int[] {i, j, 1}); // 위치 저장 및 방문
-            v[i][j] = true;
+            q.offer(new int[] {row,col,1});
+            v[row][col] = true;
 
             while (!q.isEmpty()) {
-                int[] ij = q.poll();
-                int ni = ij[0];
-                int nj = ij[1];
-                int distance = ij[2];
+                int[] current = q.poll();
+                int distance = current[2];
 
-                if (ni == row - 1 && nj == col - 1) { // 목표 지점 도달
+                if (current[0] == m-1 && current[1] == n-1) {
                     return distance;
                 }
 
-                for (int d = 0; d < 4; d++) {
-                    int newi = di[d] + ni;
-                    int newj = dj[d] + nj;
+                for (int d=0; d<4; d++) {
+                    int ii = current[0] + di[d];
+                    int jj = current[1] + dj[d];
 
-                    if (newi >= 0 && newi < row && newj >= 0 && newj < col && !v[newi][newj] && maps[newi][newj] == 1) {
-                        v[newi][newj] = true;
-                        q.offer(new int[]{newi, newj, distance + 1});
+                    if (ii >= 0 && ii < m && jj >= 0 && jj < n &&maps[ii][jj] == 1 && !v[ii][jj]) {
+                        q.offer(new int[] {ii,jj, distance+1});
+                        v[ii][jj] = true;
                     }
                 }
             }
-
-            return -1; // 도달할 수 없는 경우
+            return -1;
         }
     }
+
