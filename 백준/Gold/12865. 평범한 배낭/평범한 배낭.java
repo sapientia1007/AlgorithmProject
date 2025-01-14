10a93e8be7cbde609e1cdaa678939a6011f77498
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2025-01-14 17:44:29 +0900

    [Gold V] Title: 평범한 배낭, Time: 132 ms, Memory: 14676 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,21 +1,18 @@
 import java.io.*;
 import java.util.*;
-/*
-입력 :
-	N : 물품의 수 / K : 준서가 버틸 수 있는 무게
-	N개의 줄 -> 각 물건의 무게 W / 해당 물건 가치 V
-출력 :
-	한 줄에 배낭에 넣을 수 있는 물건들의 가치합 최대값
- */
+
 public class Main{
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int N = Integer.parseInt(st.nextToken()); int K = Integer.parseInt(st.nextToken());
-		int[] weight = new int[N]; int[] value = new int[N];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int N = Integer.parseInt(st.nextToken()); // 물품의 수
+        int K = Integer.parseInt(st.nextToken()); // 버틸 수 있는 무게
+
+        int[] value = new int[N]; int[] weight = new int[N];
+        
 		for (int i=0; i<N; i++){
-			st = new StringTokenizer(br.readLine(), " ");
+            st = new StringTokenizer(br.readLine());
 			int w = Integer.parseInt(st.nextToken()); int v = Integer.parseInt(st.nextToken());
 			weight[i] = w; value[i] = v;
 		}
@@ -30,5 +27,6 @@ public class Main{
 			}
 		}
 		System.out.println(dp[K]);
+        br.close();
 	}
 }
