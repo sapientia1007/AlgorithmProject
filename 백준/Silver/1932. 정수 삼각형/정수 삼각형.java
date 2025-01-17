f728b9d1803b39f1fde417790dad9fe5e05e0e0a
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2025-01-17 16:26:03 +0900

    [Silver I] Title: 정수 삼각형, Time: 244 ms, Memory: 27260 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,24 +1,19 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main{
-    static int[][] arr;
-    static Integer[][] dp;
-    static int N;
-
+public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        N = Integer.parseInt(br.readLine());
-
-        arr = new int[N][N];
-        dp = new Integer[N][N];
-        StringTokenizer st;
+        
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        int[][] arr = new int[N][N];
+        int[][] dp = new int[N][N];
+        
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-
-            for (int j = 0; j < i + 1; j++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j <= i; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
@@ -27,19 +22,12 @@ public class Main{
             dp[N - 1][i] = arr[N - 1][i];
         }
 
-        System.out.println(find(0, 0));
-
-    }
-
-    static int find(int depth, int idx) {
-        // 마지막 행일 경우 현재 위치의 dp값 반환
-        if(depth == N - 1) return dp[depth][idx];
-
-        // 탐색하지 않았던 값일 경우 다음 행의 양쪽 값 비교
-        if (dp[depth][idx] == null) {
-            dp[depth][idx] = Math.max(find(depth + 1, idx), find(depth + 1, idx + 1)) + arr[depth][idx];
+        for (int i = N - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[i][j] = arr[i][j] + Math.max(dp[i + 1][j], dp[i + 1][j + 1]);
+            }
         }
-        return dp[depth][idx];
 
+        System.out.println(dp[0][0]);
     }
 }
