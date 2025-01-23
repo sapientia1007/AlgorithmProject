df41a34054549f9778fad3844ab768cf529dde17
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2025-01-23 15:08:03 +0900

    [Silver II] Title: 최대 힙, Time: 320 ms, Memory: 26716 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -4,18 +4,21 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
         PriorityQueue<Integer> pq = new PriorityQueue<>((o1,o2) -> Integer.compare(o2,o1));
 
         for (int i=0; i<N; i++) {
             int inpt = Integer.parseInt(br.readLine());
             if (inpt == 0) {
-                if (pq.isEmpty()) System.out.println(0);
-                else System.out.println(pq.poll());
-            }
-            else pq.offer(inpt);
+                if (pq.isEmpty()) {
+                    bw.write("0\n");
+                } else {
+                    bw.write(pq.poll() + "\n");
+                }
+            } else pq.offer(inpt);
         }
+        bw.close();
         br.close();
     }
 }
