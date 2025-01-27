e9f63cdd4a5e48880365981420e8d601968be978
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2025-01-27 16:29:20 +0900

    [Silver II] Title: 최소 힙, Time: 788 ms, Memory: 31616 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1, o2));
+        for (int i=0; i<N; i++) {
+            int inpt = Integer.parseInt(br.readLine());
+            if (inpt == 0 ) {
+                if (pq.isEmpty()) System.out.println(0);
+                else System.out.println(pq.poll());
+            } else {
+                pq.offer(inpt);
+            }
+        }
+        br.close();
+    }
+}
