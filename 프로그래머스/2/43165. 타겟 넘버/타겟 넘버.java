636dbda4957bf7c54c866a1003e7571412c18ac7
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2025-01-29 21:57:26 +0900

    [level 2] Title: 타겟 넘버, Time: 6.84 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,17 +1,16 @@
 class Solution {
-        static int answer = 0;
         public int solution(int[] numbers, int target) {
-            answer = 0;
-            dfs(numbers,target,0,0);
+            int answer = 0;
+
+            answer = dfs(numbers, target, 0, 0);
             return answer;
         }
-
-        public void dfs(int[] numbers, int target, int idx, int sum) { // 모든 경우를 탐색하기 위한 dfs
-            if (idx == numbers.length){ // numbers 배열 끝에 인덱스가 도달했으면
-                if (target == sum) answer++; // 협재 합이 타겟과 일치하면 경우의 수 증가 
-                return; // 탐색 종료
-            }
-            dfs(numbers, target, idx+1, sum+numbers[idx]); // 현재 수 더하기
-            dfs(numbers, target, idx+1, sum-numbers[idx]); // 현재 수 뺴기
+    
+    public static int dfs(int[] numbers, int target, int idx, int sum) {
+        if (idx == numbers.length) {
+            return sum == target ? 1 : 0;
         }
+
+        return dfs(numbers, target, idx+1, sum + numbers[idx]) + dfs(numbers, target, idx+1, sum - numbers[idx]);
+    }
     }
