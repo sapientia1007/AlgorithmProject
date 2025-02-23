af14a928e61c3694a2b261e32d421f8d5a06aa7d
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2025-02-23 23:56:19 +0900

    [level 2] Title: 모음 사전, Time: 4.51 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,25 +1,33 @@
-import java.util.*; 
+import java.util.*;
+import java.io.*;
 
 class Solution {
-        static ArrayList<String> list; // 모음 문자열 저장한 사전 리스트
-        String[] dict = new String[] {"A","E","I","O","U"}; // 모음 배열
+    public int solution(String word) {
+        int answer = 0; 
+        ArrayList<String> dictionary = new ArrayList<>(); // 가능한 모든 단어를 저장할 리스트
+        String[] vowels = new String[] {"A", "E", "I", "O", "U"}; // 모음 배열
 
-        public int solution(String word) {
-            int answer = 0;
+        // 모든 가능한 단어 생성
+        generateWords("", 0, dictionary, vowels);
 
-            list = new ArrayList<>();
-            dfs("", 0); // dfs 호출로 모든 가능한 문자열 생성
-
-            for (int i=0; i<list.size(); i++) { // 주어진 단어가 리스트에서 몇번째 위치에 있는지 찾기
-                if (word.equals(list.get(i))) answer = i;
+        // 생성된 단어 리스트에서 주어진 단어의 위치를 찾기
+        for (int i = 0; i < dictionary.size(); i++) {
+            if (word.equals(dictionary.get(i))) {
+                answer = i; 
+                break;
             }
-            return answer;
         }
-        public void dfs(String str, int len) {
-            list.add(str); // 현재 문자열 추가
-            if (len == 5) return; // 문자열 길이가 5이면 종료 (최대문자열길이=5)
-            for (int i=0; i<5; i++) { // 모든 모음을 현재 문자열에 추가하여 새로운 문자열 생성하는 재귀 호출
-                dfs(str+dict[i], len+1);
-            }
+
+        return answer; 
+    }
+
+    private void generateWords(String currentWord, int currentLength, ArrayList<String> dictionary, String[] vowels) {
+        dictionary.add(currentWord); // 현재까지 생성된 단어를 리스트에 추가
+        if (currentLength == 5) return; 
+
+        // 모음 배열을 순회하며 새로운 단어를 재귀적으로 생성
+        for (int i = 0; i < vowels.length; i++) {
+            generateWords(currentWord + vowels[i], currentLength + 1, dictionary, vowels);
         }
     }
+}
