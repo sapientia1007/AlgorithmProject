715d37bc4d21a48035a185f828955e1ccfde35fd
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2025-02-23 23:33:47 +0900

    [level 2] Title: 피로도, Time: 4.79 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,23 +1,27 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
-        public boolean[] visited; public int answer;
+    public int answer;
+    public boolean[] visitedDungeons; // 방문 여부
     
-        public int solution(int k, int[][] dungeons) {
-            answer = -1;
-            visited = new boolean[dungeons.length];
-            dfs(0, k, dungeons);
-            return answer;
-        }
-
-        public void dfs(int idx, int k, int[][] dungeons) {
-            for (int i=0; i<dungeons.length; i++){
-                if (!visited[i] && k >= dungeons[i][0]){ // 방문하지 않았고, 현재 피로도가 필요 피로도보다 높으면
-                    visited[i] = true;
-                    dfs(idx+1, k-dungeons[i][1], dungeons); // 재귀 -> 피로도 누적
-                    visited[i] = false;
-                }
-            }
-            answer = Math.max(answer, idx);
-        }
+    public int solution(int initialFatigue, int[][] dungeons) {
+        answer = -1;
+        visitedDungeons = new boolean[dungeons.length]; // 던전 방문 여부 배열 초기화
+        exploreDungeons(0, initialFatigue, dungeons); 
+        return answer;
     }
+
+    public void exploreDungeons(int clearedCount, int currentFatigue, int[][] dungeons) {
+        for (int i = 0; i < dungeons.length; i++) {
+            // 던전을 방문하지 않았고, 현재 피로도가 해당 던전의 최소 요구 피로도 이상인 경우
+            if (!visitedDungeons[i] && currentFatigue >= dungeons[i][0]) {
+                visitedDungeons[i] = true; 
+                exploreDungeons(clearedCount + 1, currentFatigue - dungeons[i][1], dungeons); // 재귀 호출로 다음 던전 탐험
+                visitedDungeons[i] = false; 
+            }
+        }
+        // 현재까지 클리어한 던전 수와 최대 클리어 수를 비교하여 갱신
+        answer = Math.max(answer, clearedCount);
+    }
+}
