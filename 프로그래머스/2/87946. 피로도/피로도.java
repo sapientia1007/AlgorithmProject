9815722854677483993f478c90c608fb609d6671
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2025-02-23 23:12:03 +0900

    [level 2] Title: 피로도, Time: 4.60 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,14 +1,8 @@
 import java.util.*;
 
-/*
-최소 필요 피로도 : 해당 던전을 탐험하기 위해 가지고 있어야 하는 최소한의 피로도
-소모 피로도 : 던전을 탐험한 후 소모되는 피로도
-
-k = 유저의 현재 피로도
-dungenons [최소 필요 피로도, 소모 피로도]
-*/
 class Solution {
         public boolean[] visited; public int answer;
+    
         public int solution(int k, int[][] dungeons) {
             answer = -1;
             visited = new boolean[dungeons.length];
