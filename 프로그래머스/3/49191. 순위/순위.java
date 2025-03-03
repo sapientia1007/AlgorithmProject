921bf93efca3cde2ea08e58d97697cb0102f48aa
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2025-03-03 22:29:58 +0900

    [level 3] Title: 순위, Time: 15.89 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -18,13 +18,6 @@ class Solution {
                 }
             }
 
-            for (int i=0; i<=n; i++) {
-                for (int j=0; j<=n; j++) {
-                    System.out.print(g[i][j]+ " ");
-                }
-                System.out.println();
-            }
-
             for (int i=1; i<=n; i++){
                 int game = 0;
                 for (int j=1; j<=n ;j++) {
