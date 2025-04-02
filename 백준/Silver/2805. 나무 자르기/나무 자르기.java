62f6f5c9d5d290b2b6e2cee82cf837f9e73fd04d
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2025-04-02 23:42:36 +0900

    [Silver II] Title: 나무 자르기, Time: 492 ms, Memory: 119296 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken()); 
+        int[] tree = new int[N]; int answer = 0; 
+
+        st = new StringTokenizer(br.readLine());
+
+        int maxHeight = 0; 
+        int minHeight = 0;
+
+        for (int i=0; i<N; i++) {
+            tree[i] = Integer.parseInt(st.nextToken());
+            maxHeight = Math.max(maxHeight, tree[i]);
+        }
+
+        while (minHeight <= maxHeight) {
+            int midHeight = minHeight + (maxHeight - minHeight) / 2;
+            long res = 0;
+            for (int i : tree) {
+                if (i > midHeight) res += (i-midHeight);
+            }
+
+            if (res >= M) {
+                answer = midHeight;
+                minHeight = midHeight + 1;
+            } else {
+                maxHeight = midHeight - 1;
+            }
+        }
+
+        System.out.println(answer);
+        br.close();
+    }
+}
