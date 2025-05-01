9cd7efea44f714ebba7e2bf46d4c47a665f5bdd4
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2025-05-01 23:27:41 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1172 ms, Memory: 77476 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -9,22 +9,18 @@ class Main {
         int M = Integer.parseInt(st.nextToken()); // 합을 구해야는 횟수
 
         int[] arr = new int[N];
-        int[] sumArr = new int[N];
+        int[] sumArr = new int[N+1];
         String[] inpt = br.readLine().split(" ");
         for (int i=0; i<N; i++) arr[i] = Integer.parseInt(inpt[i]);
         sumArr[0] = arr[0];
-        for (int i=1; i<N; i++) sumArr[i] = sumArr[i-1] + arr[i];
+        for (int i=1; i<=N; i++) sumArr[i] = sumArr[i-1] + arr[i-1];
 
         for (int i=0; i<M; i++) {
             st = new StringTokenizer(br.readLine());
 
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            if (start == 1) {
-                System.out.println(sumArr[end - 1]);
-            } else {
-                System.out.println(sumArr[end - 1] - sumArr[start - 2]);
-            }
+            System.out.println(sumArr[end] - sumArr[start - 1]);
         }
         br.close();
     }
