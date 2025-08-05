2fcf54dc83f62b1276f34272505b17729b4d5892
Ji Hye <91904494+sapientia1007@users.noreply.github.com>
2025-08-05 17:39:25 +0900

    [Silver IV] Title: 30, Time: 160 ms, Memory: 16276 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.java
+++ 백준/Silver/10610. 30/30.java
@@ -5,31 +5,31 @@ class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String inpt = br.readLine();
-        
-        int[] nums = inpt
-                    .chars()
-                    .map(c -> c - '0')
-                    .boxed()
-                    .sorted(Comparator.reverseOrder())
-                    .mapToInt(Integer::intValue)
-                    .toArray();
 
+        int[] count = new int[10];
         int sum = 0;
-        boolean hasZero = false; 
-        for (int num : nums) {
-            if (num == 0) hasZero = true;
-            sum += num;
+        boolean hasZero = false;
+
+        for (char c : inpt.toCharArray()) {
+            int digit = c - '0';
+            count[digit]++;
+            sum += digit;
+            if (digit == 0) hasZero = true;
         }
 
-        if (!hasZero || sum%3 != 0) {
+        if (!hasZero || sum % 3 != 0) {
             System.out.println(-1);
-        } else {
-            for (int num : nums) {
-                System.out.print(num);
-            }
-            System.out.println();
+            return;
         }
 
+        StringBuilder sb = new StringBuilder();
+        for (int i = 9; i >= 0; i--) {
+            while (count[i]-- > 0) {
+                sb.append(i);
+            }
+        }
+
+        System.out.println(sb.toString());
         br.close();
     }
 }
